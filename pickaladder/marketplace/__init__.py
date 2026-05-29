586fbb496149ab32fe9b288b0da52f9ecec9d322
80M <admarsha@gmail.com>
2026-05-28 22:44:51 -0500

    fix: clean marketplace init conflict markers

--- pickaladder/marketplace/__init__.py
+++ pickaladder/marketplace/__init__.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from flask import Blueprint
-
-bp = Blueprint("marketplace", __name__, url_prefix="/marketplace")
-
-from . import routes  # noqa: F401
-=======
 """The marketplace blueprint."""
 
 from flask import Blueprint
@@ -19,4 +12,3 @@ bp = Blueprint(
 from . import routes  # noqa: E402
 
 __all__ = ["routes"]
->>>>>>> 395736a075685dfc196237a25821dffdb0346839
